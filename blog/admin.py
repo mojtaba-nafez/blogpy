09045b5cc8c1366a804d8b19ebce49caee0d9707
mojtaba <mojtabanafez96@gmail.com>
2021-02-19 15:21:32 +0330

    add costome admin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avator', 'description']
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'category', 'created_at']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 
 # Register your models here.
